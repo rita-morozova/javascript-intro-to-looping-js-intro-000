4eb64135b1be91dbdde96e4c93a0de621699e93d
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 20:32:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,5 +17,13 @@ function whileLoop(num){
 }
 
 function doWhileLoop(i){
-  
+  var i = 0;
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < num);
 }
