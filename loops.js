efd077a8eac43edd06cf73afb668bba035139769
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 20:23:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array){
   for (let i=0; i<25; i++){
-    console.log(`I am ${i+1} strange loops.`);
+    if (i === 1){
+      console.log("I am 1 strange loop.")
+    }else{
+      console.log(`I am ${i} strange loops.`);
   }
+}
 }
